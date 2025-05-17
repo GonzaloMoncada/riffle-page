e7c768eea0be405b17a741fae7fb9f40926fbfe5
GonzaloMoncada <gonzalomoncada47@gmail.com>
2025-05-17 02:20:41 -0300

    Create function to select number; API connection pending

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,15 @@ export default function Home() {
   const handleNumberClick = (id: number) => {
     setNumbers(prevNumbers =>
       prevNumbers.map(number =>
-        number.id === id ? { ...number, confirmation: true } : number
+        number.id === id ? { ...number, confirmation: !number.confirmation } : number
       )
     );
   };
+  const handleConfirm = () => {
+    //clone object in another variable
+    const numbersToConfirm = numbers.map(number => ({ ...number }));
+    console.log(numbersToConfirm);
+  }
   return (
     <div className="overflow-y-hidden flex flex-col items-center justify-center min-h-screen w-screen bg-gray-100">
       <div className="flex flex-col md:flex-row border border-gray-300 rounded-lg h-screen shadow-lg bg-white md:w-[70vw] w-full">
@@ -25,12 +30,13 @@ export default function Home() {
               key={number.id}
               number={number}
               onNumberClick={handleNumberClick}
+              onDelete={false}
             />
           ))}
         </main>
         <aside className="flex justify-center items-center flex-col border-t border-gray-200 p-4 gap-2">
           <input type="text" name="" id="" placeholder="Juan" className="p-2 border border-gray-500" />
-          <button className="ml-2 bg-blue-500 text-white px-4 py-2 rounded">Confirmar</button>
+          <button onClick={handleConfirm} className="ml-2 bg-blue-500 text-white px-4 py-2 rounded">Confirmar</button>
         </aside>
       </div>
     </div>
--- components/riffle/NumberRiffle.tsx
+++ components/riffle/NumberRiffle.tsx
@@ -5,14 +5,36 @@ import { useState } from "react";
 type Props = {
     number: NumberRiffleProps;
     onNumberClick: (id: number) => void;
+    onDelete: boolean;
 }
 
-export default function NumberRiffle({ number, onNumberClick }: Props) {
+export default function NumberRiffle({ number, onNumberClick, onDelete}: Props) {
     const [focus, setFocus] = useState(number.confirmation);
     const handleClick = (number: NumberRiffleProps) => {
-        setFocus(true);
-        onNumberClick(number.id);
+        if (onDelete){
+
+        }
+        else{
+            setFocus(!focus);
+            onNumberClick(number.id);
+        }
     }
+    if(onDelete){
+        return (
+            <button>
+                algo
+            </button>
+        )
+        
+    }else
+    if (number.confirmation) {
+        return (
+            <button className={`border border-gray-400 bg-gray-400 flex items-center justify-center text-black`}>
+                {number.id}
+            </button>
+        )
+    }
+
     return (
         <button onClick={() => handleClick(number)} className={`cursor-pointer border border-gray-400 flex items-center justify-center text-black ${focus ? "bg-green-400" : "bg-white"}`}>
             {number.id}
